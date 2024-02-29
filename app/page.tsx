4625d3578d5bd689e82f655579ba08516bb330fc
shpetimaliu <shpetimmaliu@gmail.com>
2024-02-29 20:32:05 +0100

    func filter for place

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import ListingCard from "./components/ListingCard";
 import MapFilter from "./components/MapFilter";
 import prisma from "./lib/db";
@@ -28,7 +29,25 @@ async function getData({
   return data;
 }
 
-export default async function Home({
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: {
+    filter?: string;
+  };
+}) {
+  return (
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilter />
+
+      <Suspense key={searchParams?.filter} fallback={<p>Loading...</p>}>
+        <ShowPlace searchParams={searchParams} />
+      </Suspense>
+    </div>
+  );
+}
+
+async function ShowPlace({
   searchParams,
 }: {
   searchParams?: {
@@ -36,21 +55,19 @@ export default async function Home({
   };
 }) {
   const data = await getData({ searchParams: searchParams });
+
   return (
-    <div className="container mx-auto px-5 lg:px-10">
-      <MapFilter />
-      <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
-        {data.map((item) => (
-          <ListingCard
-            key={item.id}
-            title={item.title as string}
-            description={item.description as string}
-            imagePath={item.photo as string}
-            price={item.price as number}
-            location={item.country as string}
-          />
-        ))}
-      </div>
+    <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+      {data.map((item) => (
+        <ListingCard
+          key={item.id}
+          title={item.title as string}
+          description={item.description as string}
+          imagePath={item.photo as string}
+          price={item.price as number}
+          location={item.country as string}
+        />
+      ))}
     </div>
   );
 }
